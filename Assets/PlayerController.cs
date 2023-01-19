26d0df8ea07b1ecb77b9779ae41277f53ebf64c2
Vojtěch Staněk <vojteskas99@gmail.com>
2023-01-19 22:48:39 +0100

    Animations done

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -18,9 +19,11 @@ public class PlayerController : MonoBehaviour
     private bool _goingRight = true;
     private bool _isJumping = false;
     private bool _isFalling = false;
+    private bool _attack = false;
     private static readonly int animatorSpeedKey = Animator.StringToHash("Speed");
     private static readonly int animatorIsJumpingKey = Animator.StringToHash("IsJumping");
     private static readonly int animatorIsFallingKey = Animator.StringToHash("IsFalling");
+    private static readonly int animatorSwordSwingKey = Animator.StringToHash("SwordSwing");
 
     private void Awake()
     {
@@ -48,6 +51,14 @@ public class PlayerController : MonoBehaviour
         {
             myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpStrength);
             IsGrounded = false;
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _attack = !_attack;
         }
     }
     
@@ -81,6 +92,7 @@ public class PlayerController : MonoBehaviour
             animator.SetFloat(animatorSpeedKey, Mathf.Abs(velocity));
             animator.SetBool(animatorIsJumpingKey, myRigidBody.velocity.y > 0);
             animator.SetBool(animatorIsFallingKey, myRigidBody.velocity.y < 0);
+            animator.SetBool(animatorSwordSwingKey, _attack);
         }
     }
 
