bfde72d45487c8a53552726a3d8ed050c2d7fed1
Lukas Prokes <lukas.prokes03@gmail.com>
2023-01-21 12:11:25 +0100

    Moving camera

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject leftBorder;
+    public GameObject rightBorder;
+
+    private float left;
+    private float right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        left = leftBorder.transform.position.x + 8.7f;
+        right = rightBorder.transform.position.x - 8.7f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x > left && player.transform.position.x < right)
+        {
+            transform.position = new Vector3(player.transform.position.x,player.transform.position.y,transform.position.z);
+        }
+        
+    }
+}
