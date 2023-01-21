1fcdcd8fa9159509bb8f9fb2fe3c0becb5c798aa
Vojtěch Staněk <vojteskas99@gmail.com>
2023-01-21 16:32:53 +0100

    Fix merge error

--- Assets/Scripts/LevelHolder.cs
+++ Assets/Scripts/LevelHolder.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,6 +16,7 @@ public class LevelHolder : MonoBehaviour
     public Text levelNumberText;
     public Text levelCompletedText;
     public AudioSource deathSound;
+    public AudioSource soundtrack;
 
     private Vector3 originalPosition;
     private int activeLevelNumber;
